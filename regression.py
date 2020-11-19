03acb85c213cfbb47f7c7d4c38e14500e1c6147f
Rishikesh Khetri <rishikeshkhetri1@gmail.com>
2020-11-20 00:15:38 +0530

    Add files via upload

--- regression.py
+++ regression.py
@@ -7,8 +7,26 @@ import sys
 
 TRAIN_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
 TEST_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
+def Cost(ar,pr,th):
+    n=len(pr)
+    yp=ar.dot(th)
+    er=(yp-pr)**2
+    return 1/(2*n)(numpy.sum(er)+numpy.sum(th)**2)
 
-
+def GrD(ar,pr,th,al,itr):
+    n=len(pr)
+    costs=[]
+    for i in range(itr):
+    yp=ar.dot(th)
+    er=numpy.dot(ar.dot(area.transpose(),(yp-pr))
+    th-=al*er*(1/m)
+    costs.append(cost(ar,pr,th))
+    return costs,th;
+    
+def prediction(ar,th):
+    yp=numpy.dot(th.transpose(),area)
+    return yp
+    
 def predict_price(area) -> float:
     """
     This method must accept as input an array `area` (represents a list of areas sizes in sq feet) and must return the respective predicted prices (price per sq foot) using the linear regression model that you build.
@@ -17,6 +35,17 @@ def predict_price(area) -> float:
     """
     response = requests.get(TRAIN_DATA_URL)
     # YOUR IMPLEMENTATION HERE
+    itr=800
+    al=0.01
+    smpl=len(area)
+    print(area)
+    feat=numpy.size(len(area),1)
+    par=numpy.size((feat+1,1))
+    cof=None
+    int=None
+    th=numpy.zeros(area.shape)
+    costs,th=Grd(ar,pr,th,al,itr)
+    yd=Prediction(ar,th)
     ...
 
 
